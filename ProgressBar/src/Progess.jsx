b769694dcd53f3f0a1f98cbbd48ff83740b8f669
varruunnn <varun.choudhary2512@gmail.com>
2025-03-30 14:15:39 +0530

    pB

---
+++ ProgressBar/src/Progess.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const Progess = ({ progesss }) => {
+  const [animate, setanimate] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setanimate(progesss);
+    }, 10);
+  }, [progesss]);
+  return (
+    <div className="outer">
+      <div
+        className="inner"
+        style={{
+          width: `${animate}%`,
+          backgroundColor: animate > 5 ? "greenyellow" : "",
+        }}
+      >
+        {animate >= 5 && `${animate}%`}
+      </div>
+    </div>
+  );
+};
+
+export default Progess;
